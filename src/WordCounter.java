d350c0b514cb61cc4bb7bc8799a367d3ae48aeeb
zaccowan <zcowan@bellarmine.edu>
2024-02-20 14:42:22 -0500

    WordCounter filters files from local directory to auto collect and read all output files.

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,7 +1,24 @@
+import java.io.*;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WordCounter {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+        File rootDir = new File("./");
+        FileFilter fileFilter = file -> file.getPath().endsWith("_wc.txt");
 
+        File[] wcFileArr = rootDir.listFiles(fileFilter);
+        for(int i = 0; i < Objects.requireNonNull(wcFileArr).length ; i++ ) {
+            File currFile = wcFileArr[i];
+            BufferedReader br = new BufferedReader(new FileReader(currFile.getPath()));
+            String line = br.readLine();
+            while( line != null) {
+
+                System.out.println(line);
+
+                line = br.readLine(); // Get next line
+            }
+        }
     }
-
-
 }
