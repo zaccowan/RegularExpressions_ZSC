22b1860e056070133851b80c331f3b5c5f044436
zaccowan <zcowan@bellarmine.edu>
2024-02-20 12:28:21 -0500

    basic output file creation for specified novel.

--- src/NovelProcessor.java
+++ src/NovelProcessor.java
@@ -1,11 +1,22 @@
 import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Program to take in a novel in plain text form and a regular expressions pattern text file
+ * and output at file of all the pattern match counts for given novel.
+ * @author Zachary Cowan
+ * @version 1.0
+ * Assignment 4
+ * CS322 - Compiler Construction
+ * Spring 2024
+ */
 public class NovelProcessor {
 
     public static void main(String [] args) {
 
+        String novelName = "";
         String novelPath = "";
         String regexPath = "";
 
@@ -19,7 +30,13 @@ public class NovelProcessor {
 
         } else {
             novelPath = args[0];
-            if(!novelPath.endsWith(".txt")) novelPath += ".txt";
+            if(!novelPath.endsWith(".txt")) {
+                novelName = novelPath;
+                novelPath += ".txt";
+            }
+            else {
+                novelName = novelPath.substring(0, novelPath.length()-4);
+            }
             regexPath = args[1];
             if(!regexPath.endsWith(".txt")) regexPath += ".txt";
         }
@@ -51,5 +68,21 @@ public class NovelProcessor {
         catch (IOException e) {
             throw new RuntimeException(e);
         } // end catch
+
+        try {
+            File outputFile = new File( "./" +novelName + "_wc.txt");
+
+            if (outputFile.createNewFile()) {
+                System.out.println("File created: " + outputFile.getName());
+                FileWriter myWriter = new FileWriter(novelName + "_wc.txt");
+                myWriter.write(novelName + " is lame.");
+                myWriter.close();
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     } // end main
 } // end class
